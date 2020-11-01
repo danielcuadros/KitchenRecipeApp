0abe3631f47bc92fe132abe428bc74d17cabcc67
danielcuadros <jcuadros398@unab.edu.co>
2020-11-01 16:05:15 -0500

    Class Recipe and User

---
+++ app/src/main/java/co/edu/unab/cuadros/juan/kitchenrecipeapp/Recipe.java
@@ -0,0 +1,60 @@
+package co.edu.unab.cuadros.juan.kitchenrecipeapp;
+
+public class Recipe {
+    String id;
+    String name;
+    String user;
+    String urlImage;
+    String description;
+
+    public Recipe() {
+    }
+
+    public Recipe(String id, String name, String user, String urlImage, String description) {
+        this.id = "";
+        this.name = name;
+        this.user = user;
+        this.urlImage = urlImage;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/co/edu/unab/cuadros/juan/kitchenrecipeapp/User.java
@@ -0,0 +1,56 @@
+package co.edu.unab.cuadros.juan.kitchenrecipeapp;
+
+public class User {
+    String id;
+    String name;
+    String email;
+    String urlImage;
+
+    public User() {
+    }
+
+    public User(String name, String email, String urlImage) {
+        this.name = name;
+        this.email = email;
+        this.urlImage = urlImage;
+    }
+
+    public User(String id, String name, String email, String urlImage) {
+        this.id = "";
+        this.name = name;
+        this.email = email;
+        this.urlImage = urlImage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
